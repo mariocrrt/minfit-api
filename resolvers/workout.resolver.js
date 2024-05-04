4e892c2df92342d0e2e73aa23a2008220f03e23d
Mario Cerretti <mario.cerretti@gmail.com>
2024-05-04 12:12:43 +0200

    feat: added create workout resolver

--- resolvers/workout.resolver.js
+++ resolvers/workout.resolver.js
@@ -1,6 +1,46 @@
+import Workout from "../models/workout.model.js";
+
 const workoutResolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    workouts: async (_, __, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorized");
+
+        const userId = context.getUser()._id;
+
+        const workouts = await Workout.find({ userId: userId });
+        return workouts;
+      } catch (err) {
+        console.error("Error getting workouts", err);
+        throw new Error("Error getting workouts");
+      }
+    },
+    workout: async (_, { workoutId }) => {
+      try {
+        const workout = await Workout.findById(workoutId);
+
+        return workout;
+      } catch (er) {}
+    },
+  },
+  Mutation: {
+    createWorkout: async (_, { input }, context) => {
+      try {
+        const newWorkout = new Workout({
+          ...input,
+          userId: context.getUser()._id,
+        });
+
+        await newWorkout.save();
+        return newWorkout;
+      } catch (err) {
+        console.error("Error creating workout", err);
+        throw new Error("Error creating workout");
+      }
+    },
+    updateWorkout: async (parent, args, context) => {},
+    deleteWorkout: async (parent, args, context) => {},
+  },
 };
 
 export default workoutResolver;
